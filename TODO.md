10356ac96deffad99039f59307682bc87395a5e8
Henry Black <henry@henryblack.co>
2025-10-24 23:18:07 -0400

    Add music commands + tweaks

---
+++ TODO.md
@@ -0,0 +1,3 @@
+# TODO
+
+- [ ] Add [mkcd](https://codeberg.org/EvanHahn/dotfiles/src/commit/843b9ee13d949d346a4a73ccee2a99351aed285b/home/zsh/.config/zsh/aliases.zsh#L38-L41) to the aliases but make it work for fish as well
--- shared-shell/vars.sh
+++ shared-shell/vars.sh
@@ -1,3 +1,11 @@
+# Drives
+export HD2_DRIVE="/Volumes/HD2"
+
+
+# Code
 export DOTFILES_DIR="$HOME/dotfiles"
 export CODE_DIR="$HOME/code"
 export STOW_DIR="$DOTFILES_DIR"
+
+# Music
+export MUSIC_INBOX="$HD2_DRIVE/Music/Media/Automatically Add to Music.localized"
