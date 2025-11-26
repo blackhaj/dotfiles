9cc19a025a68d19be997cd58a7ca902d6aa8390e
Henry Black <henry@henryblack.co>
2025-11-26 15:02:50 -0500

    chore: new bot alias

--- shared-shell/work-aliases.sh
+++ shared-shell/work-aliases.sh
@@ -7,6 +7,6 @@ alias omono="code -r ~/maze/maze-monorepo"
 alias omod="code -r ~/maze/maze-monorepo/services/service-moderated-testing"
 alias web="cd ~/maze/maze-monorepo/apps/maze-webapp"
 alias tu="tilt up"
-alias pipe="cd ~/maze/maze-monorepo/services/pipecat-ai-moderator"
+alias bot="cd ~/maze/maze-monorepo/services/pipecat-ai-moderator"
 
 alias pb="pnpm bootstrap"
