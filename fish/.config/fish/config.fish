26eeca99d5c593900141c3e4ec24bcb8e456a3ec
Henry Black <henry@henryblack.co>
2026-01-22 09:14:58 -0500

    chore: move fish theme to config for 4.3 upgrade

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -64,10 +64,37 @@ end
 # 5. INTERACTIVE-ONLY SETTINGS
 # ----------------------------------------------------------------------------
 if status is-interactive
-    # Fish appearance and behavior
-    set -q fish_greeting; or set -U fish_greeting
-    set -q fish_color_command; or set -U fish_color_command 5281EB
-    set -q fish_color_quote; or set -U fish_color_quote ce9178
+    # Fish appearance
+    set -g fish_greeting
+
+    # Theme colors (migrated from fish 4.3 universal vars)
+    set -g fish_color_autosuggestion brblack
+    set -g fish_color_cancel -r
+    set -g fish_color_command 5281EB
+    set -g fish_color_comment red
+    set -g fish_color_cwd green
+    set -g fish_color_cwd_root red
+    set -g fish_color_end green
+    set -g fish_color_error brred
+    set -g fish_color_escape brcyan
+    set -g fish_color_history_current --bold
+    set -g fish_color_host normal
+    set -g fish_color_host_remote yellow
+    set -g fish_color_normal normal
+    set -g fish_color_operator brcyan
+    set -g fish_color_param cyan
+    set -g fish_color_quote ce9178
+    set -g fish_color_redirection cyan --bold
+    set -g fish_color_search_match white --background=brblack
+    set -g fish_color_selection white --bold --background=brblack
+    set -g fish_color_status red
+    set -g fish_color_user brgreen
+    set -g fish_color_valid_path --underline
+    set -g fish_pager_color_completion normal
+    set -g fish_pager_color_description yellow -i
+    set -g fish_pager_color_prefix normal --bold --underline
+    set -g fish_pager_color_progress brwhite --background=cyan
+    set -g fish_pager_color_selected_background -r
 
     # Starship prompt - OPTIMIZED: Use cached init if available
     if command -q starship
