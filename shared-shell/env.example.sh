3f339f69bf9f38c59196ef03a09c8b597a767112
Henry Black <henry@henryblack.co>
2026-05-18 14:52:50 -0400

    add stats script

--- shared-shell/env.example.sh
+++ shared-shell/env.example.sh
@@ -5,6 +5,10 @@ EDITOR='cursor'
 ## Work NPM Token
 export NPM_TOKEN=some-token
 
+## GitHub stats
+export GITHUB_ORG_NAME=your-org
+export GITHUB_TEAM_USERNAMES=user1,user2,user3
+
 export EDITOR
 
 # Aliases
